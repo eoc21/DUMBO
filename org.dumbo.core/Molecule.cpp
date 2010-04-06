486f737b2b924248bb9c212a2d61c36e2f508f81
Ed Cannon <eoc21@cam.ac.uk>
2010-04-06 11:12:25 +0100

    Simple molecule class to represent chemical molecules.

---
+++ org.dumbo.core/Molecule.cpp
@@ -0,0 +1,17 @@
+/*
+ * Molecule.cpp
+ *
+ *  Created on: Apr 6, 2010
+ *      Author: ed
+ */
+
+#include "Molecule.h"
+
+Molecule::Molecule() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Molecule::~Molecule() {
+	// TODO Auto-generated destructor stub
+}
---
+++ org.dumbo.core/Molecule.h
@@ -0,0 +1,17 @@
+/*
+ * Molecule.h
+ *
+ *  Created on: Apr 6, 2010
+ *      Author: ed
+ */
+
+#ifndef MOLECULE_H_
+#define MOLECULE_H_
+
+class Molecule {
+public:
+	Molecule();
+	virtual ~Molecule();
+};
+
+#endif /* MOLECULE_H_ */
