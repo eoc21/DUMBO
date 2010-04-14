f6a367fb1a77136faf9cbe3a0fb6923f8b53a9af
Ed Cannon <eoc21@cam.ac.uk>
2010-04-14 16:44:56 +0100

    Added extra method.

--- org.dumbo.core/Bond.h
+++ org.dumbo.core/Bond.h
@@ -7,11 +7,16 @@
 
 #ifndef BOND_H_
 #define BOND_H_
+#include "Atom.h";
 
 class Bond {
 public:
+	Bond(Atom *a1, Atom *a2);
+	Bond(Atom *a1, Atom *a2, int order);
+	Bond(Atom *a1, Atom *a2, int order, int stereoParity);
 	Bond();
 	virtual ~Bond();
+
 };
 
 #endif /* BOND_H_ */
--- org.dumbo.test/Tester2.cpp
+++ org.dumbo.test/Tester2.cpp
@@ -15,13 +15,16 @@ Tester2::Tester2() {
 Tester2::~Tester2() {
 	// TODO Auto-generated destructor stub
 }
-
+#include <algorithm>
 #include "Molecule.h"
 #include "Atom.h"
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
 	Molecule m;
 	vector<Atom*> v;
 	Atom* a;
@@ -30,4 +33,32 @@ int main(){
 	Atom *b = new Atom("blahx");
 	cout << b->getSymbol();
 	cout << m.getAtoms().size();
+	//Try reading in mol2 file
+	string aLine;
+	string moleculeStartPoint = "@<TRIPOS>MOLECULE";
+	bool moleculeFlag = false;
+	ifstream myfile (argv[1]);
+	Molecule molecule;
+	if (myfile.is_open())
+	  {
+	    while (!myfile.eof() )
+	    {
+	      getline (myfile,aLine);
+	      if(aLine.find(moleculeStartPoint) != string::npos){
+	    	  moleculeFlag = true;
+	    	   // cout << aLine;
+	      }
+	      if(moleculeFlag == true){
+	    	  //Initialize molecule!
+	      }
+
+	    }
+	    myfile.close();
+	  }
+
+	  else cout << "Unable to open file";
+
+	  return 0;
+
+
 }
